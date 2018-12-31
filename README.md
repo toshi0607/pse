fa7095945edeb419095e8679f3d859d7d838baca
toshi0607 <s.toshi0607@gmail.com>
2018-12-31 09:59:26 +0900

    :shower: remove unused code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # pse
 CLI for Cloud Pub/Sub emulator
+
+# usage
+
+1. go run main.go pub create -p testProject -t testTopic
+2. go run main.go sub create -p testProject -t testTopic -s testSubscription
+3. go run main.go pub publish -t testProject -t testTopic
+4. go run main.go sub receive -t testProject -s testSubscription
--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"os"
-	"time"
 
-	"cloud.google.com/go/pubsub"
 	"github.com/toshi0607/pse/subcmd"
 )
 
@@ -23,91 +19,4 @@ func run() error {
 		log.Fatalf("failed to find cmd: %s err:%v", os.Args[1], err)
 	}
 	return c.Run(os.Args[1:])
-}
-
-func publish() error {
-	projectID := os.Args[2]
-	subcmd := os.Args[3]
-	topicID := os.Args[4]
-
-	ctx := context.Background()
-	client, err := pubsub.NewClient(ctx, projectID)
-	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
-	}
-
-	switch subcmd {
-	// ① go run main.go pub testProject create testTopic
-	case "create":
-		topic, err := client.CreateTopic(ctx, topicID)
-		if err != nil {
-			log.Fatalf("Failed to create topic: %v", err)
-		}
-
-		fmt.Printf("Topic %v created.\n", topic)
-	// ③ go run main.go pub testProject publish testTopic
-	case "publish":
-		topic := client.Topic(topicID)
-		defer topic.Stop()
-		for i := 0; i < 10; i++ {
-			r := topic.Publish(ctx, &pubsub.Message{
-				Data: []byte(fmt.Sprintf("hello world: %d", i)),
-			})
-			id, err := r.Get(ctx)
-			if err != nil {
-				log.Fatalf("Failed to get message id: %s", err)
-			}
-			fmt.Printf("message published: %s\n", id)
-		}
-	}
-
-	return nil
-}
-
-func subscribe() error {
-	projectID := os.Args[2]
-	subcmd := os.Args[3]
-
-	ctx := context.Background()
-	client, err := pubsub.NewClient(ctx, projectID)
-	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
-	}
-
-	switch subcmd {
-	// ② go run main.go sub testProject create testTopic testSubscription
-	case "create":
-		topicID := os.Args[4]
-		subscriptionID := os.Args[5]
-
-		topic, err := client.CreateTopic(ctx, topicID)
-		if err != nil {
-			log.Fatalf("Failed to create topic: %v", err)
-		}
-
-		sub, err := client.CreateSubscription(ctx, subscriptionID, pubsub.SubscriptionConfig{
-			Topic:       topic,
-			AckDeadline: 10 * time.Second,
-		})
-		if err != nil {
-			log.Fatalf("Failed to create subscription: %v", err)
-		}
-
-		fmt.Printf("Subscription %v created.\n", sub)
-
-	// ④ go run main.go sub testProject receive testSubscription
-	case "receive":
-		subscriptionID := os.Args[4]
-
-		sub := client.Subscription(subscriptionID)
-		err = sub.Receive(ctx, func(ctx context.Context, m *pubsub.Message) {
-			fmt.Printf("message received: %v\n", string(m.Data))
-			m.Ack()
-		})
-		if err != context.Canceled {
-			log.Fatalf("Failed to reveive message: %v", err)
-		}
-	}
-
-	return nil
 }
