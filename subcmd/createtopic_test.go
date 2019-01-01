110ab27657cf1cac21e563a243e92bfbbfb21495
toshi0607 <s.toshi0607@gmail.com>
2019-01-01 14:22:58 +0900

    :green_heart: add createtopic test

--- subcmd/createtopic_test.go
+++ subcmd/createtopic_test.go
@@ -8,20 +8,20 @@ import (
 	"github.com/toshi0607/pse/mock/pubsub"
 )
 
-func TestCreateSub_NewCreateSub(t *testing.T) {
+func TestCreateTopic_NewCreateSub(t *testing.T) {
 	t.Run("create CreateSub successfully", func(t *testing.T) {
-		s := NewCreateSub()
+		s := NewCreateTopic()
 		if s == nil {
 			t.Errorf("CreateSub should be created")
 		}
 	})
 }
 
-func TestCreateSub_Name(t *testing.T) {
+func TestCreateTopic_Name(t *testing.T) {
 	t.Run("show a sub command's name successfully", func(t *testing.T) {
-		want := "create-sub"
+		want := "create-topic"
 
-		s := NewCreateSub()
+		s := NewCreateTopic()
 		got := s.Name()
 		if got != want {
 			t.Errorf("got: %s, want: %s", got, want)
@@ -29,11 +29,11 @@ func TestCreateSub_Name(t *testing.T) {
 	})
 }
 
-func TestCreateSub_Summary(t *testing.T) {
+func TestCreateTopic_Summary(t *testing.T) {
 	t.Run("show a sub command's summary successfully", func(t *testing.T) {
-		want := "Create subscription"
+		want := "Create topic"
 
-		s := NewCreateSub()
+		s := NewCreateTopic()
 		got := s.Summary()
 		if got != want {
 			t.Errorf("got: %s, want: %s", got, want)
@@ -41,10 +41,10 @@ func TestCreateSub_Summary(t *testing.T) {
 	})
 }
 
-func TestCreateSub_Run(t *testing.T) {
+func TestCreateTopic_Run(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-	fsub := mock_pubsub.NewMockSubscriber(ctrl)
+	fpub := mock_pubsub.NewMockPublisher(ctrl)
 
 	tests := map[string]struct {
 		init      func()
@@ -53,9 +53,9 @@ func TestCreateSub_Run(t *testing.T) {
 	}{
 		"normal": {
 			func() {
-				fsub.EXPECT().Init(gomock.Any(), "testp").Return(nil)
-				fsub.EXPECT().CreateSubscription(gomock.Any(), "tests", "testt").Return(nil, nil)
-			}, []string{"create-sub", "-p", "testp", "-t", "testt", "-s", "tests"}, false,
+				fpub.EXPECT().Init(gomock.Any(), "testp").Return(nil)
+				fpub.EXPECT().CreateTopic(gomock.Any(), "testt").Return(nil, nil)
+			}, []string{"create-sub", "-p", "testp", "-t", "testt"}, false,
 		},
 		"with help option": {
 			func() {},
@@ -63,36 +63,32 @@ func TestCreateSub_Run(t *testing.T) {
 		},
 		"without project ID": {
 			func() {},
-			[]string{"create-sub", "-t", "testt", "-s", "tests"}, true,
+			[]string{"create-sub", "-t", "testt"}, true,
 		},
 		"without topic ID": {
 			func() {},
-			[]string{"create-sub", "-p", "testp", "-s", "tests"}, true,
-		},
-		"without subscription ID": {
-			func() {},
-			[]string{"create-sub", "-p", "testp", "-t", "testt"}, true,
+			[]string{"create-sub", "-p", "testp"}, true,
 		},
 		"with Subscriber.Init error": {
 			func() {
-				fsub.EXPECT().Init(gomock.Any(), "testp").Return(errors.New("test error"))
+				fpub.EXPECT().Init(gomock.Any(), "testp").Return(errors.New("test error"))
 			},
-			[]string{"create-sub", "-p", "testp", "-t", "testt", "-s", "tests"}, true,
+			[]string{"create-sub", "-p", "testp", "-t", "testt"}, true,
 		},
 		"with Subscriber.CreateSubscription error": {
 			func() {
-				fsub.EXPECT().Init(gomock.Any(), "testp").Return(nil)
-				fsub.EXPECT().CreateSubscription(gomock.Any(), "tests", "testt").Return(nil, errors.New("test error"))
+				fpub.EXPECT().Init(gomock.Any(), "testp").Return(nil)
+				fpub.EXPECT().CreateTopic(gomock.Any(), "testt").Return(nil, errors.New("test error"))
 			},
-			[]string{"create-sub", "-p", "testp", "-t", "testt", "-s", "tests"}, true,
+			[]string{"create-sub", "-p", "testp", "-t", "testt"}, true,
 		},
 	}
 
 	for name, te := range tests {
 		t.Run(name, func(t *testing.T) {
 			te.init()
-			cmd := NewCreateSub()
-			cmd.sub = fsub
+			cmd := NewCreateTopic()
+			cmd.pub = fpub
 
 			if err := cmd.Run(te.args); err != nil {
 				if !te.wantError {
